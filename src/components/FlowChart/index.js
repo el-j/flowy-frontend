bb4825406f1d554ade04f543157a011648b6dc04
Fabian Althaus <fabian.althaus@altran.com>
2020-04-09 20:50:29 +0200

    add lodash

--- src/components/FlowChart/index.js
+++ src/components/FlowChart/index.js
@@ -1,21 +1,25 @@
 import React from "react";
+
 import { FlowChartWithState, INodeDefaultProps } from "@mrblenny/react-flow-chart";
+import { cloneDeep, mapValues } from 'lodash'
 import CustomNode from './Nodes/CustomNode'
 import CustomInnerNode from './Nodes/CustomNode/CustomInnerNode'
+import CustomPort from './Nodes/CustomNode/CustomPort'
 
 const FlowChart = React.forwardRef((props,ref) => {
-  let chartData = props.chartData
+  let chartData = cloneDeep(props.chartData)
     return (<FlowChartWithState
-              ref={ref}
-              initialValue={chartData}
-              config={{
-              }}
-              Components={{
-                Node: CustomNode,
-                NodeInner:CustomInnerNode
-              }}
+      ref={ref}
+      Components={{
+        Node: CustomNode,
+        NodeInner:CustomInnerNode,
+        // Port: CustomPort
+      }}
+      // config={{ smartRouting: true }}
+      initialValue={chartData}
               />
   )
 })
+
 
 export default FlowChart
