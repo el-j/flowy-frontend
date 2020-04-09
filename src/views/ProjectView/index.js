d115b03f685575af12dfba21ac1313f22f0f5699
Fabian Althaus <fabian.althaus@altran.com>
2020-04-09 20:50:01 +0200

    add overlay and sidebar features to project view

--- src/views/ProjectView/index.js
+++ src/views/ProjectView/index.js
@@ -5,6 +5,9 @@ import mermaid from 'mermaid'
 import FlowChart from '../../components/FlowChart'
 import Options from '../../components/ProjectOptions'
 import useFetchApi from '../../components/fetchApi/useFetchApi.js'
+import NewItemOverlay from '../../components/newItemOverlay'
+import actions from "@mrblenny/react-flow-chart";
+import { cloneDeep, mapValues } from 'lodash'
 
 const emptyProject = (name) => {
   return ({name: name,
@@ -21,6 +24,9 @@ const emptyProject = (name) => {
   }})
 }
 
+
+
+
 const ProjectView = (props) => {
       const myprojectName = props.projectName.slice(1)
       const apiUrl = `loadProject/:${myprojectName}`
@@ -28,6 +34,33 @@ const ProjectView = (props) => {
       const [error,setError] = useState(null)
       const [isLoaded,setIsLoaded]= useState(false)
       const [project,setProject]= useState()
+
+      const [newItem,setNewItem]= useState({
+        id: "node1",
+        type: "input-output",
+        position: {
+          x: 300,
+          y: 100
+        },
+        ports: {
+          port1: {
+            id: "port1",
+            type: "input",
+            properties: {
+              value: "yes"
+            }
+          },
+          port2: {
+            id: "port2",
+            type: "output",
+            properties: {
+              value: "no"
+            }
+          }
+        }
+      })
+      const [newItemCreate,setNewItemCreate]= useState(false)
+      const itemRef = React.createRef();
       const flowchartRef = React.createRef();
 
       useEffect(() => {
@@ -49,7 +82,8 @@ const ProjectView = (props) => {
         }
       },[loadProject])
 
-      const handleClick = ()=>{ console.log("yes")}
+      const handleClick = ()=>{
+         console.log("yes")}
 
       const handleSave = (e) => {
         let response = {}
@@ -68,6 +102,99 @@ const ProjectView = (props) => {
           })
       }
 
+      const handleChange = event => {
+        // console.log(event.currentTarget.id,event.currentTarget.value,newItem)
+        let id = event.currentTarget.id
+        let portAmount = Object.keys(newItem.ports).length+1
+        let ports = newItem.ports
+        let thisPortId = `port${portAmount}`
+        let temp = {}
+        switch (id) {
+          case 'newInputPortname':
+
+            break;
+          case 'newInputProperties':
+          console.log('change Portname:',event.currentTarget.value,newItem);
+          temp = {[thisPortId]:{
+              id: thisPortId,
+              type: "input",
+            properties: {
+              value: event.currentTarget.value
+            }}
+            }
+
+            setNewItem({ports:{...temp,...ports}})
+            break;
+          default:
+
+        }
+      }
+      const handleConfigureNode = (e) => {
+        setNewItemCreate(true)
+        console.log(e)
+      }
+
+      const stateActions = mapValues(actions, (any) => {
+        console.log(actions);
+        return( (...args: any) => {
+          console.log(args,any)
+          setProject(...args)
+
+        }
+      )
+    })
+
+
+
+
+const handleDeletePort = port => {
+  console.log(port)
+  let ports = newItem.ports
+  let allPorts = Object.keys(ports)
+  let temp = delete ports[port]
+  console.log(temp,ports);
+  setNewItem({...newItem,ports:{...ports}})
+}
+
+const handleAdd  = event => {
+  let id = event.currentTarget.id
+  let portAmount = Object.keys(newItem.ports)
+  if (!portAmount.length <= 0) {
+    portAmount = Math.max(...portAmount.toString().match(/\d+/g)) +1
+  }
+  else {
+    portAmount = 1
+  }
+  console.log(portAmount);
+  let ports = newItem.ports
+  let thisPortId = `port${portAmount}`
+  let temp = {}
+  let type = ''
+  switch (id) {
+    case 'addInput':
+        type='input'
+      break;
+    case 'addOutput':
+        type='output'
+      break;
+    default:
+
+  }
+
+    temp = {[thisPortId]:{
+        id: thisPortId,
+        type: type,
+      properties: {
+        value: ''
+      }}
+      }
+
+      ports = {...ports,...temp}
+      setNewItem({...newItem,ports:{...ports}})
+      console.log('change Portname:',newItem);
+
+  }
+
       if (error) {
         return <div>Error: {error.message}</div>;
       } else if (!isLoaded) {
@@ -77,8 +204,28 @@ const ProjectView = (props) => {
         <div className='container-fluid'>
               {project&&project.projectJson?(
                 <>
-                  <Options handleSave={handleSave} items={project.projectJson.nodes}/>
-                  <FlowChart id={'projectFlowGraph'} ref={flowchartRef} chartData={project.projectJson}/>
+                {console.log("we rerender",newItem,actions)}
+                  <NewItemOverlay
+                    handleAdd={handleAdd }
+                    handleDeletePort={handleDeletePort }
+                    handleChange={handleChange}
+                    handleClose={() =>setNewItemCreate(false)}
+                    node={{...newItem}}
+                    submit={'value'}
+                    itemRef={itemRef}
+                    show={newItemCreate}
+                  />
+                  <Options
+                    itemRef={itemRef}
+                    handleSave={handleSave}
+                    newItem={newItem}
+                    handleConfigureNode={handleConfigureNode}
+                    items={project.projectJson.nodes}
+                    />
+                  <FlowChart id={'projectFlowGraph'}
+                    stateActions={stateActions}
+                    ref={flowchartRef}
+                    chartData={project.projectJson}/>
                 </>
                 ):(null)}
         </div>
