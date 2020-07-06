3f03f21700f034ac75d9a2473d5369c580a7c186
el-j <bluearc.in.sky@gmail.com>
2020-07-06 17:34:13 +0200

    working onclick for leftpanel

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -132,6 +132,8 @@ const Sidebar = ({
           {
             // console.log(newItem, selected)
           }
+
+          {selected.type === 'node' ? (<>
         <PictureRow>
           <img src={newItem.path} style={{
             width: '100%'
@@ -148,9 +150,6 @@ const Sidebar = ({
               onChange={e => handleChange(e,newItem.id)}
 
             />
-
-
-
           </PictureRowTitle>
         </Row>
         <Row>
@@ -167,7 +166,7 @@ const Sidebar = ({
             />
           </PictureRowTitle>
         </Row>
-
+        </>):<h1>Decicsion</h1>}
         <StyledNodePreviewRow as={Row}>
           <Col lg={{span: 3}}>
               <h6>Inputs</h6>
