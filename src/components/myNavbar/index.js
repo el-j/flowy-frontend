c7434f1331ed7087983087c3b83906950f1a7ba0
falthaus <fabian.althaus@altran.com>
2020-10-20 17:11:39 +0200

    updated navbar to appbar

--- src/components/myNavbar/index.js
+++ src/components/myNavbar/index.js
@@ -1,29 +1,120 @@
 import React from "react";
-import { Navbar, NavbarBrand,Form, Button, Input } from 'reactstrap';
-import styled from 'styled-components';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 
-const Fixit = styled.div`
-  position:fixed;
-  max-width: 350px;
-  z-index:1000;
-  opacity: 0.5;
-`
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  fixed: {
+    position: 'fixed',
+    maxWidth: '350px',
+    zIndex:1000,
+    opacity: 0.5,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
 
 const MyNavbar = ({handleSearch,projectName}) => {
+    const classes = useStyles();
+
   if (projectName) {
-    return(<Fixit>
-      <Navbar dark>
-        <NavbarBrand href="/" className="mr-auto">{projectName}</NavbarBrand>
-      </Navbar>
-    </Fixit>  )
+    return(
+      <AppBar position="static">
+          <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            {projectName}
+          </Typography>
+
+        </Toolbar>
+      </AppBar>
+  )
   }
-  return (<div><Navbar dark expand="lg">
-      <NavbarBrand href="/">flowy</NavbarBrand>
-      <Form inline>
-              <Input type="search" placeholder="Search" id="searchBar" className="mr-lg-2" onChange={handleSearch}/>
-              <Button variant="outline-info">Search</Button>
-      </Form>
-    </Navbar></div>)
+  return (<div className={classes.root}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="open drawer"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography className={classes.title} variant="h6" noWrap>
+          flowy
+        </Typography>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </div>
+      </Toolbar>
+    </AppBar>
+  </div>)
 }
 
 export default MyNavbar
